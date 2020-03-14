dccb8807414a5a100e3c6bab4874b6e8d85665af
Lukas Karlsson <karlsson@broadinstitute.org>
2020-03-14 15:32:30 +0000

    Updated image.

--- bucket_to_bigquery/main.py
+++ bucket_to_bigquery/main.py
@@ -4,12 +4,15 @@
 # import google.auth
 
 # from google.cloud import bigquery
-# from google.cloud import storage
+from google.cloud import storage
 
 
 def bucket_to_bigquery(request):
     """Move GitHub CSV to Bucket."""
-    print('hello world!')
+    bucket_name = 'broad-covid-19'
+    client = storage.Client()
+    for blob in client.list_blobs(bucket_name):
+        print(blob)
 
 
 if __name__ == '__main__':
